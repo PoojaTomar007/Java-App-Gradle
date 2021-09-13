b1574f7cb3367a6de21753cfc4b7cc4f76d070f6
PoojaTomar007 <90127609+PoojaTomar007@users.noreply.github.com>
2021-09-13 16:51:00 +0530

    Update execGradle.groovy

--- vars/execGradle.groovy
+++ vars/execGradle.groovy
@@ -1,6 +1,3 @@
 def call(String options = 'test') {
   sh "/opt/gradle/gradle-6.4.1/bin/gradle ${options}"
 }
-def call(String options = 'build'){
-sh "/opt/gradle/gradle-6.4.1/bin/gradle ${options}"
-}
